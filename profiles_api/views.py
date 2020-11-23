8b088656ce21fa7fedd9b027ef49568e3c2421d1
Raghav <ragnap99@gmail.com>
2020-11-23 19:12:57 +0530

    added profiles

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,11 +3,11 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import viewsets
+from rest_framework import filters
 
 from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
-...
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating, creating and updating profiles"""
@@ -15,7 +15,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
-    
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+        
 class HelloApiView(APIView):
     """Test API View"""
 
